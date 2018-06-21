3afd83f3b8421d4dba901070205db3c1befbc2e2
savanipoojan78 <savanipoojan78@gmail.com>
2018-06-21 16:39:45 +0530

    Add the Computer,Science,Politics etc. Activity

---
+++ app/src/main/java/com/example/poojan/udacity_quiz_app/Computer.java
@@ -0,0 +1,16 @@
+package com.example.poojan.udacity_quiz_app;
+
+import android.app.ActionBar;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Computer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer);
+
+    }
+}
--- app/src/main/java/com/example/poojan/udacity_quiz_app/MainActivity.java
+++ app/src/main/java/com/example/poojan/udacity_quiz_app/MainActivity.java
@@ -1,17 +1,105 @@
 package com.example.poojan.udacity_quiz_app;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 private Window window;
+ImageView Computer;
+ImageView Gk;
+ImageView Sport;
+ImageView Science;
+ImageView Politics;
+ImageView Geography;
+ImageView GameOfthrones;
+ImageView HarryPotter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         window=getWindow();
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        setup();
+        Click();
+
     }
+    void setup()
+    {
+        Computer=(ImageView)findViewById(R.id.computer);
+        Gk=(ImageView)findViewById(R.id.gk);
+        Sport=(ImageView)findViewById(R.id.sport);
+        Science=(ImageView)findViewById(R.id.science);
+        Politics=(ImageView)findViewById(R.id.politics);
+        Geography=(ImageView)findViewById(R.id.geography);
+        GameOfthrones=(ImageView)findViewById(R.id.got);
+        HarryPotter=(ImageView)findViewById(R.id.harry);
+    }
+    void Click()
+    {
+        Computer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Computer.class);
+
+                startActivity(i);
+            }
+        });
+        Gk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,GK.class);
+
+                startActivity(i);
+            }
+        });
+        Sport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Sport.class);
+                startActivity(i);
+            }
+        });
+        Science.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Science.class);
+                startActivity(i);
+            }
+        });
+        Politics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Politics.class);
+                startActivity(i);
+            }
+        });
+        Geography.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Geography.class);
+                startActivity(i);
+            }
+        });
+        GameOfthrones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,GameOfThrones.class);
+                startActivity(i);
+            }
+        });
+        HarryPotter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,HarryPotter.class);
+                startActivity(i);
+            }
+        });
+    }
+
 }
